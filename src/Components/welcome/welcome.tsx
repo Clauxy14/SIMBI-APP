49a330e9d132ba472483d0442764285718682b6a
clauxy14 <onyinyechibenedicta500@gmail.com>
2025-05-15 03:57:22 -0700

    resolved build error

--- src/Components/welcome/welcome.tsx
+++ src/Components/welcome/welcome.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import WelcomeMobile from "./welcomeMobil";
 import WelcomeDesktop from "./welcomeDesktop";
 
